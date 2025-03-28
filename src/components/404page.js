6f19c6c894c9dd094c0066eabcda791095491ad0
tenor2000 <tenor2000@gmail.com>
2025-03-28 14:18:08 -0400

    finished 404 page

--- src/components/404page.js
+++ src/components/404page.js
@@ -1,8 +1,20 @@
 function pageNotFound(mainDiv) {
-  const page = document.createElement("h1");
-  page.textContent =
-    "Sorry you have stumbled into a place of ill repute. Nothing to see here.";
+  const page = document.createElement("div");
+  page.classList = "d-flex flex-column m-auto p-5 text-center";
 
+  const h1 = document.createElement("h1");
+  h1.textContent = "Were you looking for Cat Photos?";
+
+  const para = document.createElement("p");
+  para.textContent = "Sorry, this page doesn't exist. Maybe next time.";
+
+  const image = document.createElement("img");
+  image.src = "https://i.gifer.com/2GU.gif";
+  image.alt = "cat furiously typing";
+
+  page.appendChild(h1);
+  page.appendChild(image);
+  page.appendChild(para);
   mainDiv.appendChild(page);
 }
 
